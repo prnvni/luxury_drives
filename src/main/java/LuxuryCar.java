d442cb15f1b8154b5440e9113020ef6ef128312b
prnvni <95880351+prnvni@users.noreply.github.com>
2025-11-26 00:32:07 +0400

    Update LuxuryCar.java

--- src/main/java/LuxuryCar.java
+++ src/main/java/LuxuryCar.java
@@ -1,11 +1,13 @@
-
+//inherit the abstract Car class
 public class LuxuryCar extends Car {
+    // additional feature of luxury car: mileage
     private double mileage;
-
+    //constructor
     public LuxuryCar(int id,String brand, String model, int year, String engine, String transmission,
                      double price, String category, String color, String interior,
                      String drivetrain, String fuelType, String features, String image,
                      double mileage) {
+        //call parent constructor
         super(id, brand, model, year, engine, transmission, price, category, color, interior,
                 drivetrain, fuelType, features, image);
         this.mileage = mileage;
@@ -13,8 +15,10 @@ public class LuxuryCar extends Car {
 
     @Override
     public String toString() {
+        //return json to front end and close the bracket
         return super.toString()
                 + ",\"mileage\":" + mileage
                 + "}";
     }
 }
+
