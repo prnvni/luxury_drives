43a26a71a3e0a8b45b552351ace4764fb93c9e0c
prnvni <95880351+prnvni@users.noreply.github.com>
2025-11-26 00:33:35 +0400

    comments
    Added comments for clarity and improved readability.

--- src/main/java/SUVCar.java
+++ src/main/java/SUVCar.java
@@ -1,14 +1,13 @@
-
-
+//inherit abstract Car class        
 public class SUVCar extends Car {
-
+    //extra feature of SUVCar : seating capacity
     private int seatingCapacity;
-
+    //constructor
     public SUVCar(int id,String brand, String model, int year, String engine, String transmission,
                   double price, String category, String color, String interior,
                   String drivetrain, String fuelType, String features, String image,
                   int seatingCapacity) {
-
+        //call parent constructor
         super(id,brand, model, year, engine, transmission, price,
                 category, color, interior, drivetrain, fuelType, features, image);
 
@@ -17,8 +16,10 @@ public class SUVCar extends Car {
 
     @Override
     public String toString() {
+        //return json format 
         return super.toString()
                 + ",\"seatingCapacity\":" + seatingCapacity
                 + "}";
     }
 }
+
