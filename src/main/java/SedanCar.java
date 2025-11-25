1412111c71a089effa048c754558734a87f3c483
prnvni <95880351+prnvni@users.noreply.github.com>
2025-11-26 00:34:44 +0400

    comments for clarity
    Added comments for clarity and maintained functionality.

--- src/main/java/SedanCar.java
+++ src/main/java/SedanCar.java
@@ -1,11 +1,13 @@
-
+//inherit abstract Car class
 public class SedanCar extends Car {
+    //extra feature of Sedan car:  trunk space
     private int trunkSpace;
-
+    //constructor
     public SedanCar(int id,String brand, String model, int year, String engine, String transmission,
                     double price, String category, String color, String interior,
                     String drivetrain, String fuelType, String features, String image,
                     int trunkSpace) {
+        //call parent constructor
         super(id, brand, model, year, engine, transmission, price, category, color, interior,
                 drivetrain, fuelType, features, image);
         this.trunkSpace = trunkSpace;
@@ -13,8 +15,10 @@ public class SedanCar extends Car {
 
     @Override
     public String toString() {
+        //return json format response
         return super.toString()
                 + ",\"trunkSpaceLiters\":" + trunkSpace
                 + "}";
     }
 }
+
