d799358f0a5742509c160ae21b3ee1cc0751ee39
prnvni <95880351+prnvni@users.noreply.github.com>
2025-11-26 10:36:21 +0400

    Add mileage getter and improve LuxuryCar class
    Added a getter method for mileage in LuxuryCar class.

--- src/main/java/LuxuryCar.java
+++ src/main/java/LuxuryCar.java
@@ -2,23 +2,27 @@
 public class LuxuryCar extends Car {
     // additional feature of luxury car: mileage
     private double mileage;
-    //constructor
+
     public LuxuryCar(int id,String brand, String model, int year, String engine, String transmission,
                      double price, String category, String color, String interior,
                      String drivetrain, String fuelType, String features, String image,
                      double mileage) {
-        //call parent constructor
+          //call parent constructor
         super(id, brand, model, year, engine, transmission, price, category, color, interior,
                 drivetrain, fuelType, features, image);
         this.mileage = mileage;
     }
+    public double getMileage() {
+    	return this.mileage;
+    }
 
     @Override
+            //return json to front end and close the bracket
     public String toString() {
-        //return json to front end and close the bracket
         return super.toString()
                 + ",\"mileage\":" + mileage
                 + "}";
     }
 }
+
 
