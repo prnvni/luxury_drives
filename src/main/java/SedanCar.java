37f9574485f71786bdec18ba77d16f2bdc87dded
prnvni <95880351+prnvni@users.noreply.github.com>
2025-11-26 10:37:30 +0400

    Add getter for trunkSpace in SedanCar class
    Added a getter method for trunkSpace.

--- src/main/java/SedanCar.java
+++ src/main/java/SedanCar.java
@@ -1,24 +1,28 @@
+
 //inherit abstract Car class
 public class SedanCar extends Car {
-    //extra feature of Sedan car:  trunk space
+        //extra feature of Sedan car:  trunk space
     private int trunkSpace;
-    //constructor
+ //constructor
     public SedanCar(int id,String brand, String model, int year, String engine, String transmission,
                     double price, String category, String color, String interior,
                     String drivetrain, String fuelType, String features, String image,
                     int trunkSpace) {
-        //call parent constructor
+         //call parent constructor
         super(id, brand, model, year, engine, transmission, price, category, color, interior,
                 drivetrain, fuelType, features, image);
         this.trunkSpace = trunkSpace;
     }
 
+    
     @Override
     public String toString() {
-        //return json format response
         return super.toString()
                 + ",\"trunkSpaceLiters\":" + trunkSpace
                 + "}";
     }
+            //return json format response
+	public int gettrunkSpace() {
+		return this.trunkSpace;
+	}
 }
-
